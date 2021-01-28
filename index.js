bea1371b4f8b24343f890e1ea3233dd858a41d81
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2021-01-28 14:40:56 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+// newHeader.setAttribute('id','victory')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'MARCOS is the champion'
+
+document.body.appendChild(newHeader)
